8e144816dec8ed0cfad7eb1976bed4ead4be8168
keimoriyama <86696267+keimoriyama0097@users.noreply.github.com>
2022-04-07 06:38:23 +0000

    add preprocessing class

--- code/data/shinra_utils.py
+++ code/data/shinra_utils.py
@@ -4,16 +4,34 @@ import tqdm
 import os
 
 
+class FileUtils:
+    def __init__(self, label_path, wiki_path):
+        self.label_path = label_path
+        self.wiki_path = wiki_path
+
+    def load_labeldata(self):
+        return self.load_shinra_json(self.label_path)
+
+    def load_shinra_json(self, path):
+        data = []
+        with open(self.label_path, "r") as f, \
+                tqdm.tqdm(desc=os.path.basename(path)) as t:
+            for d in map(json.loads, f):
+                data.append(d)
+                t.update()
+        return data
+
+
 def main():
     print("hello")
-    path = "./trial_en/trial_en/en/en_ENEW_LIST.json"
+    path = "./trial_en/trial_en/en/en-trial-wiki-20190121-cirrussearch-content.json"
     data = []
     with open(path, "r") as f, \
             tqdm.tqdm(desc=os.path.basename(path)) as t:
         for d in map(json.loads, f):
             data.append(d)
             t.update()
-    print(data)
+    print(data[0])
 
 
 if __name__ == "__main__":
