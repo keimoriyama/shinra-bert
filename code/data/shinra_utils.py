5744a4e616d062874068fe6b0a669cb80f534f14
keimoriyama <86696267+keimoriyama0097@users.noreply.github.com>
2022-04-11 07:30:45 +0000

    finished labeling

--- code/data/shinra_utils.py
+++ code/data/shinra_utils.py
@@ -1,7 +1,7 @@
 import json
-from collections import OrderedDict
 import tqdm
 import os
+import pandas as pd
 
 
 class FileUtils:
@@ -11,11 +11,11 @@ class FileUtils:
         self.wiki_path = self.base_path + wiki_name
         self.limit = limit
 
-    def load_labeldata(self):
-        return self.load_shinra_json(self.label_path)
+    def load_labeldata(self, limit):
+        return self.load_shinra_json(self.label_path, limit)
 
-    def load_wikidata(self):
-        wikidata = self.load_shinra_json(self.wiki_path, True)
+    def load_wikidata(self, limit):
+        wikidata = self.load_shinra_json(self.wiki_path, limit)
         return self.merge_wikidata(wikidata)
 
     def merge_wikidata(self, wikidata):
@@ -40,9 +40,33 @@ class FileUtils:
         return data
 
 
-class ShinraData():
-    def __init__(self) -> None:
-        pass
+def label_preprocess(labels):
+    df = pd.DataFrame()
+    label_list = []
+    for label in tqdm.tqdm(labels, desc = "extracting page id and page name"):
+        label_dict = {'pageid': label['pageid'], 'title': label['title']}
+        enes = label['ENEs']
+        label_ene_dict = {}
+        for ene in enes:
+            label_ene_dict = dict(**label_dict, **ene)
+            label_list.append(label_ene_dict)
+    df = pd.DataFrame(label_list)
+    return df
+
+
+def wikidata_preprocess(wikidata):
+    df = pd.DataFrame()
+    wiki_dict_list = []
+    for wiki in tqdm.tqdm(wikidata, desc = "extracting wiki data"):
+        # print(wiki.keys())
+        wikidict = {"type": wiki['index']['_type'],
+                    "id": wiki['index']['_id'],
+                    "text": wiki['text']}
+        wiki_dict_list.append(wikidict)
+    wiki_df = pd.DataFrame(wiki_dict_list)
+    df = pd.concat([df, wiki_df])
+    return df
+# 実装のテスト　
 
 
 def main():
@@ -51,10 +75,12 @@ def main():
     label_name = "en_ENEW_LIST.json"
     wiki_name = "en-trial-wiki-20190121-cirrussearch-content.json"
     files = FileUtils(base_path, label_name, wiki_name)
-    labels = files.load_labeldata()
-    wikidata = files.load_wikidata()
-    # print(type(labels[0]), type(wikidata[0]))
-    print(wikidata[0])
+    labels = files.load_labeldata(False)
+    wikidata = files.load_wikidata(True)
+    label_df = label_preprocess(labels)
+    wiki_df = wikidata_preprocess(wikidata)
+    label_df.to_csv("./csv/labels.csv", index=False)
+    wiki_df.to_csv("./csv/wiki.csv", index=False)
 
 
 if __name__ == "__main__":
--- code/preprocess.py
+++ code/preprocess.py
@@ -1,8 +1,9 @@
 import os
 import argparse
-
-import multiprocessing as multiprocessing
-from data.shinra_utils import ShinraData
+from data.shinra_utils import FileUtils, label_preprocess, wikidata_preprocess
+import pandas as pd
+from transformers import AutoTokenizer
+from tqdm import tqdm
 
 
 def load_arg():
@@ -10,8 +11,65 @@ def load_arg():
     return parser.parse_args()
 
 
+def gen_data():
+    label_path = "/data/csv/labels.csv"
+    wiki_path = "/data/csv/wiki.csv"
+    cwd = os.getcwd()
+    base = "."
+    if '/workspaces/shinra-bert' == cwd:
+        base = "./code/"
+    dir_path = base + "/data/csv/"
+    label_path = base + label_path
+    wiki_path = base + wiki_path
+
+    if not os.listdir(dir_path):
+        base_path = base + "/data/trial_en/trial_en/en/"
+        label_name = "en_ENEW_LIST.json"
+        wiki_name = "en-trial-wiki-20190121-cirrussearch-content.json"
+        files = FileUtils(base_path, label_name, wiki_name)
+        labels = files.load_labeldata(False)
+        wikidata = files.load_wikidata(True)
+        label_df = label_preprocess(labels)
+        label_df.to_csv(label_path, index=False)
+        del label_df, labels
+        wiki_df = wikidata_preprocess(wikidata)
+        wiki_df.to_csv(wiki_path, index=False)
+        del wikidata, wiki_df
+
+    label_df = pd.read_csv(label_path)
+    wiki_df = pd.read_csv(wiki_path)
+    return label_df, wiki_df
+
+
 def main():
     args = load_arg()
+
+    label_df, wiki_df = gen_data()
+    label_list, label_pair_list = [], []
+
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+    tokenized_texts = []
+    with tqdm(total=len(wiki_df), desc="tokenizing text") as t:
+        for _, wiki in wiki_df.iterrows():
+            text = wiki['text']
+            id = wiki['id']
+            #tokenized_text = tokenizer.tokenize(text)
+            pair = {
+                "id": id,
+                "tokenized_text": text
+            }
+            tokenized_texts.append(pair)
+            t.update(1)
+    wiki_data = pd.DataFrame(tokenized_texts)
+    label_df.drop_duplicates(subset="pageid")
+
+    df = pd.merge(wiki_data, label_df, left_on='id', right_on='pageid')
+    df['label'] = 0
+
+    def get_index(x):
+        return df[df['pageid'] == x].index[0]
+    df['label'] = df['pageid'].apply(get_index)
+    print(df.head())
 
 
 if __name__ == "__main__":
