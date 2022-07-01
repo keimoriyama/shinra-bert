acfb5171438d1ba2b5d87c3043391373396996da
keimoiryama <keischwiiz@gmail.com>
2022-07-01 16:10:46 +0900

    fix preprocess

--- code/preprocess.py
+++ code/preprocess.py
@@ -1,6 +1,7 @@
 import os
 from transformers import BertTokenizer
 from tqdm import tqdm
+import json
 
 from data.shinra_utils import FileUtils, label_preprocess, wikidata_preprocess
 # cuDFを使うか〜？
@@ -22,35 +23,6 @@ class Tokenizer():
     def decode(self, text):
         ids = self.tokenizer.convert_tokens_to_ids(text)
         return self.tokenizer.decode(ids)
-
-
-
-def gen_data(debug, data_path, file_data_name, file_label_name):
-    label_path = "/data/csv/labels.csv"
-    wiki_path = "/data/csv/wiki.csv"
-    base = "."
-    label_path = base + label_path
-    wiki_path = base + wiki_path
-
-    base_path = base + data_path
-    label_name = file_label_name
-    wiki_name = file_data_name
-    files = FileUtils(base_path, label_name, wiki_name)
-    
-    if os.path.isfile(label_path):
-        label_df = pd.read_csv(label_path)
-    else:
-        labels = files.load_labeldata()
-        label_df = label_preprocess(labels)
-        label_df.to_csv(label_path, index=False)
-    if os.path.isfile(wiki_path):
-        wiki_df = pd.read_csv(wiki_path)
-        wiki_df["text"] = wiki_df["text"].astype(str)
-    else:
-        wikidata = files.load_wikidata(debug)
-        wiki_df = wikidata_preprocess(wikidata)
-        wiki_df.to_csv(wiki_path, index=False)
-    return label_df, wiki_df
 
 
 def split_list(target, max_len):
@@ -95,25 +67,37 @@ def generate_train_data(wiki_df,label_df, data_type, tokenizer,max_len):
     return df, labels
 
 def read_data(debug, data_path, file_data_name, file_label_name, data_type, tokenizer, max_len=512):
-    label_df, wiki_df = gen_data(debug, data_path, file_data_name,file_label_name)
+    utils = FileUtils(data_path,  file_label_name, file_data_name)
     train_data_dir = f"./data/json/{data_type}/"
     train_data_name = "train_data.jsonl"
     df = pd.DataFrame()
     if debug:
         train_data_dir += str(debug) + "/"
-        wiki_df = wiki_df[:5]
     if not os.path.exists(train_data_dir):
         os.makedirs(train_data_dir)
 
     train_data_path = train_data_dir + train_data_name
+
     if os.path.isfile(train_data_path):
-        df = pd.read_json(train_data_path, orient='records',lines=True)
+        data = []
+        with open(train_data_path, "r") as f, \
+            tqdm(desc="reading data") as t:
+            for line in f:
+                file = json.loads(line)
+                data.append(file)
+                t.update()
+        df = pd.DataFrame(data)
         labels = df.filter(items = ['ENE_name', 'label'])
         df = df.filter(items=["input_ids", "attention_mask", "token_type_ids", "label"])
     else:
-        df, labels = generate_train_data(wiki_df, label_df, data_type,tokenizer, max_len)
+        wiki_data = utils.load_wikidata()
+        label_data = utils.load_labeldata()
+        wiki_df = wikidata_preprocess(wiki_data)
+        label_df = label_preprocess(label_data)
+        df, labels = generate_train_data(wiki_df, label_df, data_type, tokenizer, max_len)
         df.to_json(train_data_path, orient='records', lines=True)
         df = df.filter(items=["input_ids", "attention_mask", "token_type_ids" "label"])
+
     return df, labels
 
 def make_input(df):
@@ -136,7 +120,7 @@ def make_label_index_pair(df):
     return label_dict
 
 
-def preprocess(debug, data_path, file_data_name, file_label_name, bert_version, data_type):
+def preprocess(debug, data_path, file_label_name, file_data_name, bert_version, data_type):
     tokenizer = Tokenizer(bert_version=bert_version)
     df, labels = read_data(debug, data_path, file_data_name, file_label_name, data_type, tokenizer)
     dataset = make_input(df)
@@ -145,8 +129,8 @@ def preprocess(debug, data_path, file_data_name, file_label_name, bert_version, 
 
 
 def main():
-    preprocess(True, "/data/", "en_ENEW_LIST.json", "en-trial-wiki-20190121-cirrussearch-content.json.gz", "bert-base-cased", "all")
-    preprocess(True, "/data/", "en_ENEW_LIST.json", "en-trial-wiki-20190121-cirrussearch-content.json.gz", "bert-base-cased", "entire")
+    # preprocess(True, "/data/", "en_ENEW_LIST.json", "en-trial-wiki-20190121-cirrussearch-content.json.gz", "bert-base-cased", "all")
+    preprocess(True, "/data/", "en-trial-wiki-20190121-cirrussearch-content.json.gz", "en_ENEW_LIST.json", "bert-base-cased", "entire")
 
 
 if __name__ == "__main__":
